b9a134134a79c986f967724222ea73493b0a5b63
Adrian Dawid <adriandwd@gmail.com>
2023-04-06 15:49:06 +0200

    Add minimal working example

---
+++ examples/minimal.jl
@@ -0,0 +1,18 @@
+using TopologyPlots
+using PGFPlotsX
+
+axis = @pgf  Axis({
+    "view" = (0,30),
+    axis_equal,
+    "scale" = 2,
+    axis_lines = "none"});
+
+TopologyPlots.plot_cylinder_bb(2, axis)
+
+ts = -2:1e-2:2
+xs = cos.(π * ts)
+ys = sin.(π * ts)
+zs = ts
+
+TopologyPlots.plot_curve_bb(xs,ys,zs,axis)
+pgfsave("minimal_example.pdf", axis)
